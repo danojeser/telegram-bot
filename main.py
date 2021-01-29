764bc2369b0802f9774e3017a387c3a8b385bb52
Daniel Ojeda <danojeser@gmail.com>
2021-01-30 00:24:04 +0100

    Command mention all.
    Cleaned a bit.

--- main.py
+++ main.py
@@ -18,62 +18,61 @@ firebase_admin.initialize_app(cred)
 firestore_db = firestore.client()
 
 
-
-
 # COMANDOS
 def bop(update, context):
     print('Ejecutanto bop')
-    url = get_image_url()
-    chat_id = update.effective_chat.id
-    user_id = str(update.effective_user.id)
-
-    print('USER ID: ' + user_id)
-
     logger('bop', update.effective_user.id, update.effective_chat.id)
-    registerUser(update.effective_chat.title, update.effective_chat.id, update.effective_user.first_name, update.effective_user.id)
-    print(update.effective_user.id)
-    context.bot.send_photo(chat_id=chat_id, photo=url)
+    register_user(update.effective_chat.title, update.effective_chat.id, update.effective_user.first_name, update.effective_user.id)
 
+    url = get_image_url()
+
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
 
 
 def chilling(update, context):
     print('Ejecutando Chilling')
-    message='Estoy de chilling'
-    chat_id = update.effective_chat.id
+    logger('chilling', update.effective_user.id, update.effective_chat.id)
+    register_user(update.effective_chat.title, update.effective_chat.id, update.effective_user.first_name, update.effective_user.id)
 
+    chat_id = update.effective_chat.id
     users_list = firestore_db.collection(u'chats').document(str(chat_id)).get().get('users')
-    print(users_list)
 
     mentions = ''
     for user_id in users_list:
-        mentions = "  " + mentions + "[" + users_list[user_id] + "](tg://user?id=" + user_id + ")" + "   "
+        if str(update.effective_user.id) != user_id:
+            mentions = " " + mentions + "[" + users_list[user_id] + "](tg://user?id=" + user_id + ")" + "   "
 
+    message = update.effective_user.first_name + ': ' + os.getenv('CHILLING_MESSAGE')
     message = message + mentions
-    """
-    TODO: implementar firebase y guardar una tabla con los participantes, despues recorrer los participantes
-    telegram_user = update.effective_user
-    user_name = telegram_user.first_name
-    user_id = telegram_user.id
-    mention = "["+user_name+"](tg://user?id="+str(user_id)+")"
-    text = 'Hola ' + mention
-    """
     context.bot.send_message(chat_id=chat_id, text=message, parse_mode="Markdown")
 
 
+def mention_all(update, context):
+    print('Ejecutando all')
+    logger('mention_all', update.effective_user.id, update.effective_chat.id)
+
+    chat_id = update.effective_chat.id
+    users_list = firestore_db.collection(u'chats').document(str(chat_id)).get().get('users')
+
+    mentions = ''
+    for user_id in users_list:
+        if str(update.effective_user.id) != user_id:
+            mentions = "  " + mentions + "[" + users_list[user_id] + "](tg://user?id=" + user_id + ")" + "   "
+
+    message = 'Ey! ' + mentions
+    context.bot.send_message(chat_id=chat_id, text=message, parse_mode="Markdown")
 
 
 # HANDLER
 def listen(update, context):
     print('Ejecutando Listen')
-    loggerMessage(update.effective_user.id, update.effective_chat.id, update.message.text)
+    logger_message(update.effective_user.id, update.effective_chat.id, update.message.text)
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 
 def unknown(update, context):
     print('Ejecutando Unknown')
     context.bot.send_message(chat_id=update.effective_chat.id, text='No se que me estas contando crack')
-
-
-
 
 
 # FUNCIONES
@@ -82,12 +81,12 @@ def logger(command, user, group):
     firestore_db.collection(u'logger').add({'command': command, 'user': user, 'group': group, 'date': time.time()})
 
 
-def loggerMessage(user, group, text):
+def logger_message(user, group, text):
     print('LOGGER MESSAGE')
     firestore_db.collection(u'loggerMessage').add({'text': text, 'user': user, 'group': group, 'date': time.time()})
 
 
-def registerUser(chat_name, chat_id, user_name, user_id):
+def register_user(chat_name, chat_id, user_name, user_id):
     print('REGISTER USER')
     doc = firestore_db.collection(u'chats').document(str(chat_id)).get()
     if doc.exists:
@@ -105,17 +104,13 @@ def registerUser(chat_name, chat_id, user_name, user_id):
         print('grupo creado')
 
 
+# def mention_all():
 
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
     return url
-
-
-
-
-
 
 
 def main():
@@ -125,17 +120,17 @@ def main():
 
     bop_handler = CommandHandler('bop', bop)
     chilling_handler = CommandHandler('chilling', chilling)
+    all_handler = CommandHandler('all', mention_all)
     listen_handler = MessageHandler(Filters.text & (~Filters.command), listen)
 
     unknown_handler = MessageHandler(Filters.command, unknown)
 
-
     dp.add_handler(bop_handler)
     dp.add_handler(chilling_handler)
+    dp.add_handler(all_handler)
     dp.add_handler(listen_handler)
 
     dp.add_handler(unknown_handler)
-
 
     updater.start_polling()
     updater.idle()
@@ -150,10 +145,8 @@ def get_image_url():
     return url
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
 
 
-
-
-#TODO:Handler gente que entra y sale del grupo
+# TODO: Handler gente que entra y sale del grupo
