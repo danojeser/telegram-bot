a9eeb7de8602c605bcc199c9de4ad9ba7d2f920e
Daniel Ojeda <danojeser@gmail.com>
2021-11-24 20:39:44 +0100

    Start change codebase to nodejs

---
+++ src/commands.js
@@ -0,0 +1,8 @@
+
+
+export class Commands {
+
+    static bop() {
+
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+
+// Comando /bop
+bot.onText(/\/bop/, function onAudioText(msg) {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'guau guau madafaca');
+});
+
+
+
+
+
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+
